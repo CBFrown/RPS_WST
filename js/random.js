b391a428f2a427fe5f752deb95d2da68f4ffa2f6
CBFrown <baalclark12@gmail.com>
2023-05-08 07:34:33 -0700

    almost done

--- js/random.js
+++ js/random.js
@@ -6,17 +6,22 @@ function rockb(){
     if (random == 0) {
         result = 'rock';
         alert(`The result is ${result}. Its a tie`);
+        document.getElementById('opponet').src='rrr.png'
+        
     }
     else if (random == 1) {
         result = 'paper';
         alert(`The result is ${result}. You lost`);
+        document.getElementById('opponet').src='ppp.png'
     }
     else if (random == 2){
         result = 'scissor';
         alert(`The result is ${result}. You won`);
+        document.getElementById('opponet').src='sss.png'
     }
     else{
         alert(`The result is ${result}. An error had occured`);
+        document.getElementById('opponet').src='bot.png'
     }
     console.log(result);
 }
